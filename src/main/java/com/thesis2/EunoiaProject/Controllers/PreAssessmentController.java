36d19a1ee740b0557167c817bb9f2a177784e77b
Nathaniel Caballero <nathaniel.caballero@ciit.edu.ph>
2025-03-25 00:31:17 +0800

    cors updated

--- src/main/java/com/thesis2/EunoiaProject/Controllers/PreAssessmentController.java
+++ src/main/java/com/thesis2/EunoiaProject/Controllers/PreAssessmentController.java
@@ -12,12 +12,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "https://eunoia.social")
 @RestController
 @RequestMapping("/api/pre-assessment")
 public class PreAssessmentController {
--- src/main/java/com/thesis2/EunoiaProject/Controllers/UserController.java
+++ src/main/java/com/thesis2/EunoiaProject/Controllers/UserController.java
@@ -7,12 +7,9 @@ import com.thesis2.EunoiaProject.Services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "https://eunoia.social")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
